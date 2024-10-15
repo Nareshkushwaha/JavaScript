bd46cb2db052271a29ee85a779a1c97701c48a4f
naresh <nareshkushwaha8003@gmail.com>
2024-10-15 16:40:07 +0530

    second js

--- second.js
+++ second.js
@@ -3,3 +3,17 @@
  let b=89;
 console.log(a+b);
 console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a*b);
+console.log(a**b);
+
+//**************************************/
+
+// Unary oprator 
+//   let a=5;
+//   let b=2;
+//    console.log("a  =" ,a, " & b = ",b);
+//    a++ //6
+
+//    console.log("a =",a)

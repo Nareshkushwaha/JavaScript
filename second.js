1cf608d157d78d9c4d3bac5ec151453997db5716
naresh <nareshkushwaha8003@gmail.com>
2024-10-15 22:19:52 +0530

    multiply

--- second.js
+++ second.js
@@ -22,8 +22,160 @@
 
 // Assignment operator 
 
-  let a =5;
-  let b=2;
+//   let a =5;
+//   let b=2;
 
-   a-=4; //a =a**4;
-   console.log("a = ",a)//9
+//    a-=4; //a =a**4;
+//    console.log("a = ",a)//9
+// // *************************************************
+   let c =5;
+   let d=2;
+ 
+    c*=4; //a =a**4;
+    console.log("c = ",c)
+
+// comperison operator in javascript
+
+// let a=5;
+// let b=3;
+
+//  console.log("5 <=3", a<b); 
+//  it return the valu e true and false
+
+// ******************************************************
+
+
+// logical operatro in javascripts
+
+// let  a=6;
+// let b=5;
+// console.log("cond1 && cond2 =" , a>b || a===6);
+
+  
+
+// let  a=6;
+// let b=5;
+// console.log("!(6>5) =" , !(a===6));
+
+   
+
+// ************************************************************\
+
+// condition statments in javascripts 
+
+//  let age =19;
+
+//   if(age>=18){
+//     console.log("you can  vote")
+// }
+//  if(age<18){
+//      console.log("you cannot vote");
+//  }
+
+// ****************************************
+
+// let mode ="dark";
+// let color;
+
+// if(mode==="dark"){
+
+//    color ="black";
+// }
+// if(mode ==="light"){
+// color="white";
+// }
+//  console.log(color);
+ 
+// *************************************
+
+// let mode ="dark";
+// let color;
+
+// if(mode==="dark"){
+
+//    color ="black";
+// }
+// else {
+// color="white";
+// }
+//  console.log(color);
+ 
+// **********************************************
+
+// let age =18;
+
+//   if(age>=18){
+//     console.log("you can  vote")
+// }
+//  else {
+//      console.log("you cannot vote");
+//  }
+
+// ***************************************
+
+//  let a=39;
+//  if(a%2==0){
+//      console.log("even number");
+//  }
+//  else{
+//      console.log("odd number");
+//  }
+   
+// *********************************************
+
+// condition statement  if, esle if, else  if
+
+//  let mode ="pink";
+//    let color;
+//   if(mode==="dark"){
+//      color="black";
+//   }
+//    else if(mode==="pink"){
+//      color="pink";
+//    }
+//     else if(mode==="white"){
+//          color="white";
+//     }
+//      else {
+//         color="yellow";
+//      }
+//       console.log(color);
+
+//        if(mode ==="dark"){
+//          console.log(mode);
+//        }
+
+// *******************************************
+
+//  this is ternary operator in javascript 
+
+//  let age=38;
+//    let result = age>18 ? "adult":"not adult";
+//     console.log(result);
+
+// ****************************************************
+
+// this is practic question in java scripts 
+
+//  let  score =50;
+//  let grad;
+//   if(score>=90&&score<=100){
+//     grad="A";
+// }
+//   else if(score >=70&& score<=89){
+//     grad="B";
+//   }
+//    else if(score>=60&& score<=69){
+//     grad="C";
+//    }
+
+//     else if(score>=50&& score<=59){ 
+//         grad="D";
+//     }
+//      else if(score>=0&& score<=49){
+//          grad="Fail"
+//      }
+     
+//          console.log(grad);
+     
+

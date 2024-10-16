4e1c8f17c58055ed2c004734ceb611aabb5cb7fa
naresh <nareshkushwaha8003@gmail.com>
2024-10-16 17:47:49 +0530

    third class java script

---
+++ third.js
@@ -0,0 +1,121 @@
+//W eare learning  loops in java scripts 
+
+//  for( let i=1;i<=5;i++){
+//     console.log("apna college");
+//  }
+//   console.log("Loops has ended");
+
+// ********************************************
+
+//  let sum =0; 
+//   let n=100;
+//   for(let i=1;i<=n;i++){
+//     sum=sum+i; //sum=15
+
+//   }
+//   console.log(sum);
+//   console.log("Loops has ended");
+
+//********************************************* */
+
+// for( let i=1;i<=5;i++){
+//         console.log(i);
+//      }
+//       console.log("Loops has ended");
+
+// *************************************
+
+// while loops in java script 
+
+// let i=1;
+// while(i<=10){
+//      console.log("apna college");
+//      i++;
+// } 
+
+
+// ******************************
+// Do while loops is java script 
+
+//  let i=1;
+//   do{
+//      console.log("i= "+i);
+//      i++;
+//   }
+//     while(i<=10);
+
+
+// ************************************
+
+//   for of for loops in java script
+
+//    let str="apna college";
+//     ;let size=0;
+//     for(let  i of str){
+//          console.log(i);
+//          size++;
+//     }  
+//      console.log("srtring size " + size); 
+
+
+// **********************************************
+// for in loops in java script
+
+//    let students={
+//      name:"rahul kumar",
+//      age:20,
+//      cgpa:7.5,
+//      ispass:true,
+//    };
+
+//     for(let  key in students){
+//          console.log("key=" ,key,"value =",students );
+//     }
+
+// lets  practic Question one
+
+//  let n=100;
+//   for(let i=1;i<=n;i++){
+//      console.log("i ="+i)
+//   }
+
+
+// print the even number from 0 to 100
+
+//  let n=100;
+//    for(let i=0;i<=n;i++){
+//      if(i%2==0){
+//          console.log("This is even number = " + i)
+//      }
+//    }
+//    console.log("Program is terminated");
+
+// Practic Question second
+
+//  create a game where  you start any random game number ask the user to keep gussing 
+//   the game numner until the user enters correct value 
+
+
+// =====================================================================
+// string in java script 
+
+
+
+//  let str="apna college";
+//  let str2='naresh kushwaha';
+//  console.log(str);
+//  console.log(str2);
+//  console.log(str.length);
+//  console.log(str2[6]);
+
+// ======================================================== 
+// let str="apna college";
+ 
+//  console.log(str[8]);
+
+let sentence ='this is  temple  literle string';
+console.log(typeof sentence);
+
+/// this is templet /string in java script
+
+

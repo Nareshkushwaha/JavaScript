0aa09cfd0b290008b7a1a123efca4a0ffc19a8c7
naresh <nareshkushwaha8003@gmail.com>
2024-10-20 18:16:10 +0530

    Array in java script

---
+++ Array.js
@@ -0,0 +1,106 @@
+//  let  marks =[1,2,3,4,5,6,7,8,9];
+//  console.log(marks);
+//  console.log(marks.length);
+
+
+//   let  heros=["ironman","thor","hulk","antmna","spidermna"];
+
+//    console.log(heros);
+
+
+//Looping over an Array
+
+// let  heros=["ironman","thor","hulk","antmna","spidermna"];
+//  for(let i=0;i<heros.length;i++){
+
+//      console.log(heros[i]+" ");
+//  }
+
+
+ //print the value using for  of loops 
+  
+  
+// let  heros=["ironman","thor","hulk","antmna","spidermna"];
+//    for(let hero of heros){
+
+//      console.log(hero+" ");
+//  }
+
+
+// let  cities=["delhi","pune","mumbai","hydrabad","gurgaon"];
+//    for(let city of cities){
+
+//      console.log(city.toUpperCase());
+//  }
+
+
+
+//lets practic question in java scripts 
+ 
+//for a given array with marks  of students =[85,97,44,37,76,60]
+//find the avrage marks of the entire class 
+
+
+//  let marks=[85,97,44,37,76,60];
+//   let sum =0;
+//    for(let val of marks){
+//     sum+=val;
+//    } 
+//  let avg=sum/marks.length;
+//  console.log('avg marks of the class =${avg}');
+//  console.log(sum);
+
+//practic question  in java script
+
+
+//   let value =[250,645,300,900,50];
+   
+//    for(let i=0;i<value.length;i++){
+//         let offer=value[i]/10;
+//         value[i]-=offer;
+//    }
+//     console.log(value); 
+
+
+
+///array some method 
+
+ //pop method 
+
+
+//   let foodItem=["pptato","apple","litchi","tomato"];
+
+// foodItem.push("banna","prages","oinun");
+// console.log(foodItem);
+
+//  let deletitem = foodItem.pop();
+
+//   console.log(foodItem);
+//   console.log("deleted",deletitem.pop);
+
+//to convert to string in java script 
+
+
+ 
+// let foodItem=["pptato","apple","litchi","tomato"];
+
+// foodItem.push("banna","prages","oinun");
+// console.log(foodItem);
+// console.log(foodItem.toString());
+// // console.log(foodItem);
+
+
+//this is concate method in array an java script 
+
+
+//  let marvel_heros=['thor',"spiderman","ironman"];
+
+//   let dcHeros= ["superman","batman"];
+
+//    let indianHeros=["shaktiman","krish"];
+   
+//   let heros =marvel_heros.concat(dcHeros,indianHeros);
+//    console.log(heros);  
+
+
+

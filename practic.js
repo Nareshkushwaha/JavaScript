906786bf8f65260b3af8c713d0e1725258663070
naresh <nareshkushwaha8003@gmail.com>
2024-10-22 18:22:33 +0530

    pratic of java script

---
+++ practic.js
@@ -0,0 +1,61 @@
+// function myfunction (a,b)
+// {
+//      console.log("This is my java script code \n"+a+b);
+// }
+//  myfunction("this is addtion "+5,8);
+ 
+//   function  my(a,b){
+//       return a*b;
+//   }
+//     console.log("This is multipication " +my(4,8));
+    
+
+//===================================================================
+//  let a=[1,2,3,4,4,5,6];
+//     let sum=a.forEach((val,index)=>
+//   {
+//         console.log(val,index);
+//   });
+      
+//====================================================================
+//  let a=["hello","hii","kese","ho","app"];
+//     let sum=a.forEach((val,index,a)=>
+//   {
+//         console.log(val,index,a);
+//   });
+      
+      //==============================================================
+    //   let num=[1,2,3,4,5,6,7,8];
+    //     num.forEach((num)=>
+    //     {
+    //           console.log(num*num);
+    //     });
+
+//====================================================================
+//  let array=[1,2,3,4,5,6,7,8];
+//   let sum=array.map((val)=>{
+//           return val*3;
+//   });
+//   console.log(sum.toString());
+   //=================================================================
+   
+    // let fill=[1,2,4,6,7,8,5,4,8,3,7,3,7,8,2,5,7,8];
+    // let sum =fill.filter((val)=>{
+    //      return val>3;
+    // });
+    //  console.log(sum.toString());
+    
+    //================================================================
+    // /  let arr=[5,3,6,9,2,4,2,9,];
+//      const output =arr.reduce((res,cur)=>
+//      { return res>cur? res:cur;
+
+//      });
+//       console.log(output);
+    //================================================================
+    //  let find=[1,2,3,,4,6,8,9,20,19,30,48];
+    //   const out=find.reduce((res,cur)=>{
+    //       return res<cur? res:cur;
+    //   });
+    //   console.log(out);
+//=====================================================================

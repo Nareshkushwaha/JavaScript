c4d4a6adaffb40fbe68429932a64956cd15603df
naresh <nareshkushwaha8003@gmail.com>
2024-10-24 11:14:01 +0530

    DOM

---
+++ practicJava Script/script.js
@@ -0,0 +1,82 @@
+// console.log("This is hello world");
+// // alert("hello eveyone");
+
+
+//=========================================================================
+// window objects
+
+//  console.log("hello");
+//   window.console.log("hello2"); 
+
+//==========================================================================
+//   window.alert("hello2");  
+//  console.log(window.document);
+//  console.dir(document.body);
+//============================================================================
+
+
+//this is id in DOM
+
+
+//  let heading =document.getElementById("heading");
+//  console.dir(heading);
+
+//==============================================================================
+//  let heading2 =document.getElementById("heading2");
+//  console.dir(heading2);
+//=========================================================================
+
+  
+//  let headings =document.getElementsByClassName("heading-class");
+//  console.dir(headings);
+//  console.log(headings);
+
+//===========================================================================
+//   let button=document.getElementById("heding");
+//   console.log(heading);
+//    console.dir(heading);
+   
+//=================================================================================
+
+//  let pera=document.getElementsByTagName("p");
+//  console.dir(pera);
+
+//==================================================================================
+//  let elementsfrist=document.querySelector("p");
+//  console.dir(elementsfrist);
+
+//==================================================================================
+ 
+//  let allelements=document.querySelectorAll(". my-class");
+//  console.dir(allelements);
+
+
+//==============================================================================
+
+//  let div=document.querySelector("div");
+//  console.dir(div);
+
+//==========================================================================
+
+//  let heading=document.querySelector("h1");
+//  console.dir(heading);
+
+//===================================================================
+
+ // create a h2 heading elements with text  hello java script Apppedns from Apna college 
+  // Students to this text uing js 
+
+//    let h2=document.querySelector("h2");
+//    console.dir(h2.innerText);
+
+//    h2.innerText=h2.innerText+"From apna college students";
+    
+//=========================================================================
+
+ //create 3 divs with common class name " box" Accesse them & add some unique text to each of them 
+
+//    let divs=document.querySelectorAll(".box");
+// //    console.log(divs);
+//     divs[0].innerText="New unique value of 1";
+//     divs[1].innerText="New unique value of 1";`  `
+//     divs[2].innerText="New unique value of 1";
